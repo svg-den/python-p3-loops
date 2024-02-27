c7258437f63fbc15874bd09003c1c8af798445f6
svg-den <sundaywanjiru@gmail.com>
2024-02-27 12:57:41 +0300

    Done assignment

--- lib/looping.py
+++ lib/looping.py
@@ -2,12 +2,31 @@
 
 def happy_new_year():
     # code goes here!
-    pass
+  
+    count = 10
+    while count >= 1:
+        print(count)
+        count -= 1
+    print("Happy New Year!")
+happy_new_year()
+pass
 
 def square_integers(int_list):
-    # code goes here!
-    pass
+    interger_list = [nums ** 2 for nums in int_list]
+    return interger_list
+print(square_integers([4, 10, 30, 13, 500]))
+pass
 
 def fizzbuzz():
-    # code goes here!
-    pass
+    for nums in range(1, 101):
+        if(nums % 3 == 0 and nums % 5 == 0 ):
+            print("FizzBuzz")
+        elif nums % 3 == 0:
+            print("Fizz")
+        elif nums % 5 == 0:
+            print ("Buzz")
+        else:
+            print (nums)
+
+fizzbuzz()
+
